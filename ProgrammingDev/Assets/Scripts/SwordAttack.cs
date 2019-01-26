541604e58ca47cdd4ac60363833dc8f57a4f1266
Ricardo Teixeira <ricardomft@gmail.com>
2019-01-26 22:30:53 +0000

    fix of merge

--- ProgrammingDev/Assets/Scripts/SwordAttack.cs
+++ ProgrammingDev/Assets/Scripts/SwordAttack.cs
@@ -10,7 +10,8 @@ public class SwordAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerDamage = GetComponentInParent<Player>().damage;
+        player = GetComponentInParent<Player>();
+        playerDamage = player.damage;
         swordHit = GetComponent<AudioSource>();
 
     }
