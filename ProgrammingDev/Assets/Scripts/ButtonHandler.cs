1d6ddeb1e83371df1d0a74c79c9feb33e88b62b5
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-01-27 02:03:24 +0000

    Initial menu

---
+++ ProgrammingDev/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene("Level_teste_Sa");
+    }
+
+    public void credits()
+    {
+        
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+}
